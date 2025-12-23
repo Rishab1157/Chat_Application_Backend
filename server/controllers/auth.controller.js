a9be4b6cf0029e1213ef33b81b6910dd0db5a845
Rishab <rishab2110991157@gmail.com>
2025-12-23 11:32:02 +0530

    werwerwe

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -46,7 +46,7 @@ export const login = async(req,res,next) => {
 };
 
 export const logout = (req,res,next)=>{
-    res.cookie("jwt","", { maxAge: 0 }); 
+    res.cookie("jwt","", { maxAge: 0,sameSite: 'None',httpOnly: true,secure: true }); 
     return dataSender(res,{message:"success"},200)      
     
 };
